58213de2622f83757b178bab7504300887dd49c5
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-05-08 23:11:20 -0300

    Adding request and ping function

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ let config = {};
 config.pingMinutes = process.env.PING_MINUTES || 30;
 config.pingStartTime = process.env.PING_START_TIME || 6;
 config.pingEndTime = process.env.PING_END_TIME || 24;
+config.pingUrls = process.env.PING_URLS ? process.env.PING_URLS.split(',') : [];
 
 module.exports = config;
--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 require('dotenv').load();
 
+const request = require('request');
 const cron = require('node-cron');
 const config = require('./config');
 
-let pingTime = `* */${config.pingMinutes} 6-0 * * *`;
+let pingTime = `*/${config.pingMinutes} 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *`;
 
 cron.schedule(pingTime, () => {
-  console.log('running a task every minute');
+  console.log('[info] starting apps ping...')
+  pingHerokuApps(config.pingUrls);
 });
+
+function pingHerokuApp(url) {
+  if (url) {
+    console.log(`[info] ping to ${url}`);
+    request(url, (error, response, body) => {
+      console.log(`[info] status: ${response.statusCode}`);
+      if (error) {
+        return console.log(`[error] error: ${error}`);
+      }
+    });
+  }
+}
+
+function pingHerokuApps(urls) {
+  if (urls) {
+    urls.forEach((url) => pingHerokuApp(url));
+  }
+}

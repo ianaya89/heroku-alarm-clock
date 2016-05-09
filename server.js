a0606c373c1b417565096fddf22ad1e891dd3587
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-05-09 00:17:57 -0300

    Improving log info

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const config = require('./config');
 const redis = require('redis');
 const redisClient = redis.createClient(config.redisUrl);
 
-let pingTime = `*/${config.pingMinutes} 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *`;
+let pingTime = `*/${config.pingMinutes} 0,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *`;
 
 redisClient.on('error', (err) => console.log(`[error] ${err}`));
 redisClient.on('connect', (msg) => console.log(`[info] redis client connected`));
@@ -21,10 +21,10 @@ function pingHerokuApp(url) {
   if (url) {
     console.log(`[info] ping to ${url}`);
     request(url, (error, response, body) => {
-      redisClient.set(url, {
+      redisClient.set(url, JSON.stringify({
         statusCode: response.statusCode,
         date: new Date()
-      });
+      }));
 
       console.log(`[info] status: ${response.statusCode}`);
       

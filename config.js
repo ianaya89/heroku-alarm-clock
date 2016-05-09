f4ce67a5f6076a4a923670cb7f31a9384eb74839
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-05-09 02:10:22 -0300

    Adding run instructions

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 let config = {};
-console.log(USER_ID);
 
 config.pingMinutes = process.env.PING_MINUTES || 30;
 config.pingHours = process.env.PING_HOURS || '6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23';
